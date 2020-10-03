8de5f1add3f7b5ad100d3d9450bc3b789371fbc4
marcelaestradarl@gmail.com <marcelaestradarl@gmail.com>
2020-10-03 00:13:40 -0600

    Metodo para leer cadena y establecer frecuencia

--- Laboratorio3ED2/Compresor/Estructuras/ColaPrioridad.cs
+++ Laboratorio3ED2/Compresor/Estructuras/ColaPrioridad.cs
@@ -1,15 +1,61 @@
 ﻿using Compresor.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Compresor.Estructuras
 {
     public class ColaPrioridad<T> : QueueInterface<T> where T : IComparable
     {
-        public NodoCola<T>[] insert(T value)
+        int contador = 0;
+        NodoCola<T>[] colaPrioridad = new NodoCola<T>[];
+        public NodoCola<T>[] insert(FileStream archivo, T value)
         {
-            throw new NotImplementedException();
+            using var reader = new BinaryReader(archivo);
+            var buffer = new byte[2000000];
+            while (archivo.Position < archivo.Length)
+            {
+                buffer = reader.ReadBytes(2000000);
+                evaluarCadena(buffer);
+            }
+            reader.Close();
+            archivo.Close();
+            return colaPrioridad;
+        }
+        public void evaluarCadena(byte[] cadena)
+        {
+            int aux = contador;
+            NodoCola<T> nodo = new NodoCola<T>();
+            for(int i = 0; i<cadena.Length; i++)
+            {
+                //ya existe la cadena de nodos para la cola  
+                if(contador != 0)
+                {
+                    foreach(var item in cadena)
+                    {
+                        for(int j = 0; j < colaPrioridad.Length; j++)
+                        {
+                            if (item.Equals(colaPrioridad[j]))
+                            {
+                                colaPrioridad[j].prioridad++;
+                                contador++;
+                                break;
+                            }
+                        }
+                        if(contador == aux)
+                        {
+                            //colaPrioridad[colaPrioridad.Length + 1].valor = (T)item;
+                            colaPrioridad[colaPrioridad.Length + 1].prioridad++;
+                        }
+                    }
+                }
+                else //aun no existe nodo para la cola 
+                {
+                    //colaPrioridad[0].valor = cadena[0];
+                    colaPrioridad[0].prioridad++;
+                }
+            }
         }
     }
 }
--- Laboratorio3ED2/Compresor/Interfaces/QueueInterface.cs
+++ Laboratorio3ED2/Compresor/Interfaces/QueueInterface.cs
@@ -1,12 +1,13 @@
 ﻿using Compresor.Estructuras;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Compresor.Interfaces
 {
     interface QueueInterface<T> where T : IComparable
     {
-        public NodoCola<T>[] insert(T value);
+        public NodoCola<T>[] insert(File archivo, T value);
     }
 }
