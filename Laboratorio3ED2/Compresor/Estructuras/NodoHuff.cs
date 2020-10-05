f202a91ad99cee0dfb358311e50da716df757d25
marcelaestradarl@gmail.com <marcelaestradarl@gmail.com>
2020-10-05 09:33:41 -0600

    Implementacion cola

--- Laboratorio3ED2/Compresor/Estructuras/NodoHuff.cs
+++ Laboratorio3ED2/Compresor/Estructuras/NodoHuff.cs
@@ -2,11 +2,17 @@ using System;
 
 namespace Compresor.Huffman
 {
-    public class NodoHuff<T> where T : System.IComparable
+    public class NodoHuff<T> where T : IComparable
     {
         public T Value { get; set; }
         public decimal FrecPrio { get; set; }
         public NodoHuff<T> Derecho { get; set; }
         public NodoHuff<T> Inzquierdo { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            return this.Value.CompareTo(obj);
+        }
     }
+    
 }
