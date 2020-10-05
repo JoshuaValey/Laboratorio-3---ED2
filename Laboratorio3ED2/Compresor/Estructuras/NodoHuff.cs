797dc7cb07357a214dfe62b3da0b3877972fa0a3
Joshua Valey <34755202+JoshuaValey@users.noreply.github.com>
2020-10-05 09:50:22 -0600

    Update NodoHuff.cs
    Comentario implementación interfaz

--- Laboratorio3ED2/Compresor/Estructuras/NodoHuff.cs
+++ Laboratorio3ED2/Compresor/Estructuras/NodoHuff.cs
@@ -3,7 +3,7 @@ using System;
 namespace Compresor.Huffman
 {
     public class NodoHuff<T> where T : IComparable
-    {
+    {//public class NodoHuff<T>:IComparable where T : IComparable
         public T Value { get; set; }
         public decimal FrecPrio { get; set; }
         public NodoHuff<T> Derecho { get; set; }
